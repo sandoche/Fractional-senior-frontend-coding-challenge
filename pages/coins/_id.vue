c2a3cc1fd2f55ac1d01dae8ff71d3eb70423150a
Sandoche Adittane <sandoche@protonmail.com>
2022-02-26 19:46:43 +0000

    :sparkles: Coin page implemented

---
+++ pages/coins/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ marketInfo }}
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  async fetch({ store, route }) {
+    await store.dispatch('coin/fetchCoin', route.params.id)
+  },
+  fetchOnServer: false,
+  computed: {
+    ...mapGetters({
+      basicInfo: 'coin/basicInfo',
+      marketInfo: 'coin/marketInfo',
+      loading: 'coin/loading',
+      error: 'coin/error'
+    })
+  }
+}
+</script>

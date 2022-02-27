f18a6d3fbb6e31ee1be22878af3c80531c9af0c4
Sandoche Adittane <sandoche@protonmail.com>
2022-02-27 16:30:22 +0000

    :sparkles: Colors updated

--- components/CoinMarketDataItem.vue
+++ components/CoinMarketDataItem.vue
@@ -2,7 +2,18 @@
   <div class="px-4 py-5 sm:p-6">
     <dt class="text-base font-normal text-gray-900">{{ name }}</dt>
     <dd class="mt-1 flex justify-between items-baseline md:block lg:flex">
-      <div class="flex items-baseline text-2xl font-semibold text-indigo-600">
+      <div
+        :class="[
+          'flex',
+          'items-baseline',
+          'text-2xl',
+          'font-semibold',
+          {
+            'text-red-500': value < 0,
+            'text-green-500': value > 0
+          }
+        ]"
+      >
         {{ value | formatPercentage }}
       </div>
     </dd>
